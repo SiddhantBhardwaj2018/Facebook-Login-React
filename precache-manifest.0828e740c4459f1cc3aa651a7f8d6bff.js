317f312af04b85239e87ff39fd66137ace54738f
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2020-10-24 13:28:28 -0700

    Updates

---
+++ precache-manifest.0828e740c4459f1cc3aa651a7f8d6bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611cd711e7c420401adac65cc6345f25",
+    "url": "/Facebook-Login-React/index.html"
+  },
+  {
+    "revision": "06464f9a39ad6c618714",
+    "url": "/Facebook-Login-React/static/css/main.922f5d75.chunk.css"
+  },
+  {
+    "revision": "0de9483bcebaea0d1866",
+    "url": "/Facebook-Login-React/static/js/2.843e0991.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Facebook-Login-React/static/js/2.843e0991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06464f9a39ad6c618714",
+    "url": "/Facebook-Login-React/static/js/main.7ae0a9e9.chunk.js"
+  },
+  {
+    "revision": "3d7bc494425be18336df",
+    "url": "/Facebook-Login-React/static/js/runtime-main.3fea4588.js"
+  }
+]);
